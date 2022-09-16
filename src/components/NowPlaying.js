0157a334f087a9aed220d43c4e1f82a44b12738d
Catalan-Nick <catalan1222@yahoo.com>
2022-09-16 00:05:22 -0500

    re-added animation

--- src/components/NowPlaying.js
+++ src/components/NowPlaying.js
@@ -15,28 +15,26 @@ const NowPlaying = () => {
   }, [])
  
   return (
-        <div className=" d-flex overflow-x-scroll " >
-        
-        {inTheater.map(({ title, release_date, poster_path,id}) => (
-        <div key={id} className="col-lg-3 col-md-3 col-sm-6 my-3">
-        <div  className="card bg-light border-primary  mx-4 ">
-            <img
-                src={`https://image.tmdb.org/t/p/original${poster_path}`}
-                className="card-img-top"
-                alt={title}
-                onError={({ currentTarget }) => {
-                    currentTarget.onerror = null; // prevents looping
-                    currentTarget.src = 'https://media.istockphoto.com/vectors/movie-time-vector-illustration-cinema-poster-concept-on-red-round-vector-id911590226?k=20&m=911590226&s=612x612&w=0&h=HlJtSKF-fLsKFy1QJ-EVnxXkktBKNS-3jUQPXsSasYs=';
-                  }}
-                />
-            <div className="card-body">
-                <p className="card-title text-black">{title}</p>
-                <Link to={`/movies/${id}`} className="btn btn-primary">Show details</Link>
-            </div>
+    <div className=" d-flex overflow-scroll animate-stop">    
+      {inTheater.map(({ title, release_date, poster_path,id}) => (
+      <div key={id} className="col-lg-3 col-md-3 col-sm-6 my-3 animate ">
+        <div className="card bg-light border-primary  mx-4 ">
+          <img
+            src={`https://image.tmdb.org/t/p/original${poster_path}`}
+              className="card-img-top"
+              alt={title}
+              onError={({ currentTarget }) => {
+                  currentTarget.onerror = null; // prevents looping
+                  currentTarget.src = 'https://media.istockphoto.com/vectors/movie-time-vector-illustration-cinema-poster-concept-on-red-round-vector-id911590226?k=20&m=911590226&s=612x612&w=0&h=HlJtSKF-fLsKFy1QJ-EVnxXkktBKNS-3jUQPXsSasYs=';
+                }}/>
+          <div className="card-body">
+            <p className="card-title text-black">{title}</p>
+            <Link to={`/movies/${id}`} className="btn btn-primary">Show details</Link>
+          </div>
         </div>
-    </div>
+      </div>
       ))}
-        </div>
-    )
+    </div>
+  )
 }
 export default NowPlaying
